7b4814aa536ff98411091b5928e25e91db63be59
ccruiz13 <ccruiz@personalsoft.com>
2025-07-16 10:55:19 -0500

    Se añade excepciones para el modelo

--- src/domain/model/Notification.js
+++ src/domain/model/Notification.js
@@ -1,9 +1,24 @@
 const SubscriptionStatus = require('../constants/SubscriptionStatus');
+const DomainConfigurationException = require('../exceptions/DomainConfigurationException');
+const ExceptionMessages = require('../constants/ExceptionMessages');
 
 class Notification {
-    constructor({ email, phone }) {
+    constructor({ email, phone, message }) {
+
+        if (!email || typeof email !== 'string') {
+            throw new DomainConfigurationException(ExceptionMessages.EMAIL_REQUIRED_AND_STRING);
+        }
+        if (!phone || typeof phone !== 'string') {
+            throw new DomainConfigurationException(ExceptionMessages.PHONE_REQUIRED_AND_STRING);
+        }
+        if (!message || typeof message !== 'string') {
+            throw new DomainConfigurationException(ExceptionMessages.MESSAGE_REQUIRED_AND_STRING);
+        }
+
+
         this.email = email;
         this.phone = phone;
+        this.message = message;
         this.preferredChannel = SubscriptionStatus.BOTH;
     }
 

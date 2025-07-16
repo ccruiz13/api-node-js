f0f8b67b59fb404ff3cd498d5380fdaeb6841253
ccruiz13 <ccruiz@personalsoft.com>
2025-07-16 08:27:31 -0500

    Se crean servicios de input y port para adaptadores y caso de uso

--- src/domain/constants/ExceptionMessages.js
+++ src/domain/constants/ExceptionMessages.js
@@ -2,6 +2,7 @@ const ExceptionMessages = Object.freeze({
   SUBSCRIPTIONS_NOT_FOUND: 'No se encontraron suscripciones para el cliente',
   GET_SUBSCRIPTIONS_BY_CUSTOMER_ID_NOT_IMPLEMENTED_MESSAGE: 'El método getSubscriptionsByCustomerId debe ser implementado',
   SUBSCRIPTION_ADAPTER_REQUIRED_MESSAGE: 'SubscriptionAdapter es requerido',
+  SEND_NOTIFICATION_NOT_IMPLEMENTED_MESSAGE : 'Método no implementado: sendNotification'
 });
 
 module.exports = ExceptionMessages;
---
+++ src/domain/ports/input/INotificationServicePort.js
@@ -0,0 +1,15 @@
+const DomainConfigurationException = require('../../exceptions/DomainConfigurationException');
+const ExceptionMessages = require('../../constants/ExceptionMessages');
+class INotificationServicePort {
+  /**
+   * Envía notificación por el canal preferido
+   * @param {NotificationPreference} preference
+   * @param {string} message
+   * @returns {Promise<void>}
+   */
+  async sendNotification(notification, message) {
+    throw new DomainConfigurationException(ExceptionMessages.SEND_NOTIFICATION_NOT_IMPLEMENTED_MESSAGE);
+  }
+}
+
+module.exports = INotificationServicePort;
